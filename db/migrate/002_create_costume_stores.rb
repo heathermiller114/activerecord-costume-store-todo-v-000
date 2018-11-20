7798d977c143f6293cdadee2a3a1341e8d347381
Heather Miller <heather.miller114@gmail.com>
2018-11-20 01:50:49 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,6 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.
+      t.integer :num_of_employees
+      
   
 end
