61f25debdd1821eea7c0b94cc36412e808f62b92
Heather Miller <heather.miller114@gmail.com>
2018-11-20 01:42:34 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,9 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   
   def change
     create_table :costumes do |t|
-      :name
+      t.string :name
+      t.string :price
+      t.string :size
+      t.string :image_url
   end
-  
 end
