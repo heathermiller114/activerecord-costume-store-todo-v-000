8f34208300409aa38224a716b31fa1f878568889
Heather Miller <heather.miller114@gmail.com>
2018-11-20 01:49:58 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,9 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   
   def change
-    create_table :costume_stores
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.
   
 end
